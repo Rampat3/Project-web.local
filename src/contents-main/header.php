5c0061d7fd18f4a80e47020a7af2c7133a5698f8
Rampat3 <dennyfells13@gmail.com>
2024-08-06 23:06:42 +0600

    Update

---
+++ src/contents-main/header.php
@@ -0,0 +1,36 @@
+<nav class="navbar bg-dark navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                </li>
+                <div class="block-right">
+                    <li class="nav-item block-right_entrance">
+                        <button type="button" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#exampleModal1">
+                            Sign in
+                        </button>
+                    </li>
+                    <li class="nav-item block-right_register">
+                        <button type="button" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#exampleModal1">
+                            Sign up
+                        </button>
+                    </li>
+                </div>
+            </ul>
+        </div>
+    </div>
+</nav>
